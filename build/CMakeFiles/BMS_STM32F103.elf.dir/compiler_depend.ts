a48dc844d3857125d5612e1208025411d5a705f7
laobanwang676-droid <laobanwang676@gmail.com>
2026-06-04 09:21:35 +0800

    删除缓存

--- build/CMakeFiles/BMS_STM32F103.elf.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for BMS_STM32F103.elf.
--- build/CMakeFiles/flash_openocd_stlink.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/flash_openocd_stlink"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/flash_openocd_stlink.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
